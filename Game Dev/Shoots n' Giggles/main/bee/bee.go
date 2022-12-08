db9cdac5c7437e08c3ed33939d85e0135162dbd6
Szymon Pawlica <szymonq222@gmail.com>
2022-12-08 16:43:14 +0000

    gay

--- Game Dev/Shoots n' Giggles/main/bee/bee.go
+++ Game Dev/Shoots n' Giggles/main/bee/bee.go
@@ -1,6 +1,26 @@
 components {
-  id: "rocket"
-  component: "/main/rocket.script"
+  id: "bee"
+  component: "/main/bee/bee.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/sprites.atlas\"\n"
+  "default_animation: \"bee\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
   position {
     x: 0.0
     y: 0.0
@@ -21,14 +41,13 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"rockets\"\n"
-  "mask: \"bee\"\n"
-  "mask: \"blocker\"\n"
+  "group: \"bee\"\n"
+  "mask: \"rockets\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 1.0\n"
+  "      x: 0.0\n"
   "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -39,36 +58,14 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "  }\n"
-  "  data: 7.764\n"
-  "  data: 4.083\n"
-  "  data: 10.0\n"
+  "  data: 24.3705\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
   "bullet: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/assets/sprites.atlas\"\n"
-  "default_animation: \"rocket\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
     x: 0.0
