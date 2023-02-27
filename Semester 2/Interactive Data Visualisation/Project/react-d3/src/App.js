df8fdb8a8aeed4902662fc4d7614d7c36523190c
PawlicaS <szymonq222@gmail.com>
2023-02-27 15:01:49 +0000

    a

--- Semester 2/Interactive Data Visualisation/Project/react-d3/src/App.js
+++ Semester 2/Interactive Data Visualisation/Project/react-d3/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
+import * as d3 from 'd3';
 import React from 'react';
 import LineGraph from './Components/LineGraph';
 
 const App = () => {
-  const data = [
-    { date: new Date(2022, 0, 1), value1: 10, value2: 20 },
-    { date: new Date(2022, 1, 1), value1: 20, value2: 30 },
-    { date: new Date(2022, 2, 1), value1: 15, value2: 25 },
-    { date: new Date(2022, 3, 1), value1: 25, value2: 35 },
-    { date: new Date(2022, 4, 1), value1: 30, value2: 40 },
-    { date: new Date(2022, 5, 1), value1: 20, value2: 30 }
-  ];
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  React.useEffect(() => {
+    d3.json("/values.json").then((d) => {
+      setData(d);
+      setLoading(false);
+    });
+    return () => undefined;
+  }, []);
   return (
-    <div className="container">
-      <LineGraph data={data} />
+    <div className="App-container">
+      {loading && <div>loading</div>}
+      {!loading && <LineGraph data={data} />}
     </div>
   );
 };
--- Semester 2/Interactive Data Visualisation/Project/react-d3/src/Components/LineGraph.js
+++ Semester 2/Interactive Data Visualisation/Project/react-d3/src/Components/LineGraph.js
@@ -6,25 +6,42 @@ const LineGraph = ({data}) => {
 
   useEffect(() => {
     d3.select("svg").remove();
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = 600 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    var retailData = [];
+    var keyshopsData = [];
+    for (var i = 0; i < data.chartData.retail.length; i++) {
+      if (data.chartData.retail[i].y != null) {
+        if (data.chartData.retail[i].x != null) {
+          retailData.push({x: i, y: data.chartData.retail[i].y})
+        }
+      }
+    }
+    for (var i = 0; i < data.chartData.keyshops.length; i++) {
+      if (data.chartData.keyshops[i].y != null) {
+        if (data.chartData.keyshops[i].x != null) {
+          keyshopsData.push({x: i, y: data.chartData.keyshops[i].y})
+        }
+      }
+    }
+    console.log(retailData);
+    const margin = { top: 20, right: 20, bottom: 30, left: 30 };
+    const width = 1200 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
 
-    const x = d3.scaleTime()
+    const x = d3.scaleLinear()
       .range([0, width])
-      .domain(d3.extent(data, d => d.date));
+      .domain([0, d3.max(retailData, d => d.x)]);
 
     const y = d3.scaleLinear()
       .range([height, 0])
-      .domain([0, d3.max(data, d => Math.max(d.value1, d.value2))]);
-
+      .domain([0, d3.max(retailData, d => d.y)]);
+      
     const line1 = d3.line()
-      .x(d => x(d.date))
-      .y(d => y(d.value1));
+      .x(d => x(d.x))
+      .y(d => y(d.y));
 
     const line2 = d3.line()
-      .x(d => x(d.date))
-      .y(d => y(d.value2));
+      .x(d => x(d.x))
+      .y(d => y(d.y));
 
     const svg = d3.select(graphRef.current)
       .append('svg')
@@ -41,14 +58,14 @@ const LineGraph = ({data}) => {
         .call(d3.axisLeft(y));
 
     svg.append('path')
-        .datum(data)
+        .datum(retailData)
         .attr('fill', 'none')
         .attr('stroke', 'steelblue')
         .attr('stroke-width', 1.5)
         .attr('d', line1);
 
     svg.append('path')
-        .datum(data)
+        .datum(keyshopsData)
         .attr('fill', 'none')
         .attr('stroke', 'orange')
         .attr('stroke-width', 1.5)
